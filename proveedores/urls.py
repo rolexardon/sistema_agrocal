9c04f910cce1a627cf40db7d6880102d1ac41735
cmondragonr <cmondragonr@hotmail.com>
2014-05-01 16:46:41 -0600

    conflict solved

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -7,9 +7,6 @@ urlpatterns = patterns('proveedores.views',
     url(r"^$", "gestion_proveedor", name="gestion_proveedor"),
     url(r"^(?P<mod_id>\d+)/detalle$", "detalle_proveedor", name="detalle_proveedor"),
     url(r"^busca_proveedor$", "busca_proveedor", name="busca_proveedor"),
-<<<<<<< HEAD
     url(r"^ingreso_crear$", "ingreso_crear", name="ingreso_crear"),
-=======
->>>>>>> 7c6f7ebb9828b38cdb02b715888e268a54ec46f6
 
 )
